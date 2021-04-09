968d974d08ae2312aa1518dfcf39865a0e6b6be0
Thom-Ivar van Dijk <ti@Thom-Ivar.local>
2021-04-09 18:16:16 +0200

    fixes in clean() function

--- R/clean.R
+++ R/clean.R
@@ -5,7 +5,7 @@
 #' @examples
 #' clean()
 #' @export
-clean <- function(){
+clean <- function(envir = parent.frame(), list.function = ls){
   broom.mid = paste0('\t\t\t\t\t',
                      c(rep('  ||\n',8), ' /||\\\n', '/||||\\\n', '======\n', rep('||||||\n', 3)))
   spaces = sapply( c(40, 38:26), function(x) paste0(rep(' ', x), collapse = ''))
@@ -17,8 +17,7 @@ clean <- function(){
                    paste0(spaces[10], '|\\\\\\\\-\n'), paste0(spaces[11], '======\n'),
                    paste0(spaces[12:14], rep('\\\\\\\\\\\\\n', 3)))
 
-  bin = ls(envir = .GlobalEnv)
-  bin = bin[-which(bin == 'clean')]
+  bin <- list.function(envir = envir)
 
   left = T
   while (length(bin) > 0){
@@ -32,10 +31,10 @@ clean <- function(){
     if (left == T) {cat(broom.left, '\n', rep('\t', 7), next.item); left = F}
     else {cat(broom.right, '\n', rep('\t', 3), next.item); left = T}
     Sys.sleep(0.35)
-
-    rm(list = item, envir = .GlobalEnv)
     bin = bin[-1]
   }
+
+  rm(list = list.function(envir = envir), envir = envir)
   graphics.off()
   cat("\014")
 }
